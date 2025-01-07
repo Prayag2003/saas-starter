89dc9c6cd72fce50b2bb2bf03d468abd98efbb52
Prayag Bhatt <prayagbhatt2003@gmail.com>
2025-01-07 22:11:11 +0530

    feat: add Prisma client setup and update dependencies

---
+++ lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaSingleton = () => {
+    return new PrismaClient();
+}
+
+type prismaSingleton = ReturnType<typeof prismaSingleton>;
+
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient | undefined };
+
+const prisma = globalForPrisma.prisma ?? prismaSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
